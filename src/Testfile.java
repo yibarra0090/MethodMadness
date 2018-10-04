4651ecfe19aa733ca7a720eb327a7d0f214b065b
NYC <yibarra0090>
2018-10-03 23:13:13 -0400

    aaaa

--- src/Testfile.java
+++ src/Testfile.java
@@ -1,6 +1,6 @@
 public class Testfile {
     
     public static void main(String[] args) {
-
+        System.out.println(YIbarraLib.isPalindrome("racecar"));
     }
 }
--- src/YIbarraLib.java
+++ src/YIbarraLib.java
@@ -1,7 +1,12 @@
 public class YIbarraLib {
-    public static boolean isPalindrome(String strg){
-        for (int i = 0; i < strg.length(); i++){
-        strg2 = strg.substring(i,i+1);
+    public static boolean isPalindrome(String strg) {
+        for (int i = 1; i < strg.length(); i++) {
+            String strg2 += strg.substring(strg.length() - i, (strg.indexOf(strg.length()) - i));
+        }
+        if (strg2.compareTo(strg) == 0) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
