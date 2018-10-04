57d71c6079c90a31d851ce92ef6b36af20a0ea83
BT_1N3_14 <yibarra0090@bths.edu>
2018-10-04 14:50:12 -0400

    Comiitnr

--- src/YIbarraLib.java
+++ src/YIbarraLib.java
@@ -1,7 +1,14 @@
 public class YIbarraLib {
-    public static boolean isPalindrome(String strg){
-        for (int i = 0; i < strg.length(); i++){
-        strg2 = strg.substring(i,i+1);
+    public static void isPalindrome(String strg){
+        int len = strg.length();
+        String strg2 = "";
+        for (int i = 0; i < len; i++){
+        strg2 = strg.substring(len-i,(len-i)+1);
+        }
+        if (strg.equals(strg2)){
+            System.out.println("true");
+        } else {
+            System.out.println("false");
         }
     }
 }
