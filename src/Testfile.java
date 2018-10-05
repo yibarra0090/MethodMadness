bcbb13435a5b76f82532d0ae0469eec5025b9050
BT_1N3_14 <yibarra0090>
2018-10-05 14:40:18 -0400

    New!

--- src/Testfile.java
+++ src/Testfile.java
@@ -2,5 +2,6 @@ public class Testfile {
 
     public static void main(String[] args) {
         YIbarraLib.isPalindrome("racecar");
+        YIbarraLib.dateStr("mm/dd/yyyy");
     }
 }
--- src/YIbarraLib.java
+++ src/YIbarraLib.java
@@ -1,14 +1,32 @@
 public class YIbarraLib {
+    /**
+     *
+     * @param strg
+     */
     public static void isPalindrome(String strg){
         int len = strg.length();
         String strg2 = "";
         for (int i = 0; i < len; i++){
-            strg2 = strg.substring(len-i,(len-i)+1);
+            strg2 += strg.substring((len-1)-i,(len-i));
         }
+        System.out.println(strg2);
         if (strg.equals(strg2)){
             System.out.println("true");
         } else {
             System.out.println("false");
         }
     }
+
+    /**
+     *
+     * @param date
+     */
+    public static void dateStr(String date){
+        String month = date.substring(0,2);
+        String day = date.substring(3,5);
+        String year = date.substring(6);
+        String date2 = day+""+"-"+""+month+""+"-"+year;
+        System.out.println(date2);
+    }
+
 }
