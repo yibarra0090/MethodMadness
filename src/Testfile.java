a5681f705ccfb9d372181e0c84942b1b4849fb39
NYC <yibarra0090>
2018-10-07 00:48:24 -0400

    hello

--- src/Testfile.java
+++ src/Testfile.java
@@ -1,7 +1,10 @@
 public class Testfile {
 
     public static void main(String[] args) {
-        YIbarraLib.isPalindrome("racecar");
-        YIbarraLib.dateStr("mm/dd/yyyy");
+        System.out.println(YIbarraLib.isPalindrome("racecar"));
+        System.out.println(YIbarraLib.dateStr("mm/dd/yyyy"));
+        System.out.println(YIbarraLib.sumUpTo(10));
+        System.out.println(YIbarraLib.cutOut("catatonic cat","cat"));
+        System.out.println(YIbarraLib.quadSolver(1.0,0.0,1.0));
     }
 }
--- src/YIbarraLib.java
+++ src/YIbarraLib.java
@@ -3,7 +3,7 @@ public class YIbarraLib {
      *
      * @param strg
      */
-    public static void isPalindrome(String strg){
+    public static boolean isPalindrome(String strg){
         int len = strg.length();
         String strg2 = "";
         for (int i = 0; i < len; i++){
@@ -11,9 +11,9 @@ public class YIbarraLib {
         }
         System.out.println(strg2);
         if (strg.equals(strg2)){
-            System.out.println("true");
+            return true;
         } else {
-            System.out.println("false");
+            return false;
         }
     }
 
@@ -21,12 +21,43 @@ public class YIbarraLib {
      *
      * @param date
      */
-    public static void dateStr(String date){
+    public static String dateStr(String date){
         String month = date.substring(0,2);
         String day = date.substring(3,5);
         String year = date.substring(6);
         String date2 = day+""+"-"+""+month+""+"-"+year;
-        System.out.println(date2);
+        return date2;
     }
 
-}
+    /**
+     *
+     * @param n
+     * @return
+     */
+    public static int sumUpTo(int n){
+     int sum = (n*(n+1))/2;
+        return sum;
+    }
+    /**
+     *
+     * @param mainStr
+     * @param subStr
+     * @return
+     */
+    public static String cutOut(String mainStr, String subStr){
+     int ind = mainStr.indexOf(subStr);
+     mainStr = mainStr.substring(ind+subStr.length());
+     return mainStr;
+    }
+    public static String quadSolver(Double a, Double b, Double c){
+        Double det = Math.sqrt((b*b)+(-4*a*c));
+        if (det < 0.0){
+            return "The roots are imaginary number becase the determinant is a negative number";
+        } else {
+            Double x1 = (((-1*b)+det)/(2*a));
+            Double x2 = (((-1*b)-det)/(2*a));
+            return x1+""+" "+"and"+""+" "+x2+""+" "+"are real roots";
+            }
+        }
+    }
+
